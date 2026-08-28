e749b3df3b62e92f361f2d0c7244e23a6257d27c
Tom Andersson <tom.andersson@prevas.se>
2026-08-28 15:42:24 +0200

    added content to the cv-page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    site: 'https://w8bro.github.io'
+    site: 'https://w8bro.github.io',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
+
+
